183f398847163313190c24472589df2408865d20
peshmargaibrahim-cyber <peshmargaibrahim@gmail.com>
2026-06-29 18:09:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'livezon-pwa-v1';
+const CACHE_NAME = 'livezon-pwa-v3';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+  'index.html',
+  'manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        // بەکارهێنانی اینکەکان بەشێوازی سادە بۆ ڕێگریکردن لە بلۆکبوون لەلایەن جیهۆبەوە
         return cache.addAll(urlsToCache);
       })
   );
@@ -18,7 +18,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // ئەگەر لە کاشدا هەبوو پێشانی بدە، ئەگەرنا لە ئینتەرنێتەوە بیهێنە
         return response || fetch(event.request);
       })
   );
