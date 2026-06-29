b839d68d8dc6df3578274bf2aed7ee7eaf1b1094
peshmargaibrahim-cyber <peshmargaibrahim@gmail.com>
2026-06-29 17:45:34 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'livezon-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // ئەگەر لە کاشدا هەبوو پێشانی بدە، ئەگەرنا لە ئینتەرنێتەوە بیهێنە
+        return response || fetch(event.request);
+      })
+  );
+});
